4bf7d34412a85de91e3919df57c3a6b31f0cfd42
N DIWAKAR <diwakarnath51@gmail.com>
2024-02-05 21:48:30 +0530

    added routes in frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import SignIn from './pages/SignIn.jsx'
+import SignUp from './pages/SignUp.jsx'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+    </div>
   )
 }
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+      "edu-sa": ["Edu SA Beginner", "cursive"],
+      mono: ["Roboto Mono", "monospace"],
+    },
     extend: {},
   },
   plugins: [],
