ef30b62e1480b83f63f3c02c192525be037dec4a
N DIWAKAR <diwakarnath51@gmail.com>
2024-02-05 22:08:35 +0530

    created navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,13 @@ import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
 import SignIn from './pages/SignIn.jsx'
 import SignUp from './pages/SignUp.jsx'
+import Header from './components/Header.jsx'
 
 
 const App = () => {
   return (
     <div>
+      <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
