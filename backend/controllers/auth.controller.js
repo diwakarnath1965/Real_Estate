4bb8e7165b0eae4d37e8395a8f376daa0a8c5f4a
N DIWAKAR <diwakarnath51@gmail.com>
2024-02-06 18:59:36 +0530

    created signin route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const signup = async (req, res) => {
   try {
@@ -23,12 +26,50 @@ export const signup = async (req, res) => {
     return res.status(201).json({
       success: true,
       message: "Signup successfull",
-      newUser
     });
   } catch (error) {
     return res.status(401).json({
-        success:false,
-        message:"Signup failed, Please try again!!",
-    })
+      success: false,
+      message: "Signup failed, Please try again!!",
+    });
+  }
+};
+
+export const signin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "User doesn't exist",
+      });
+    }
+
+    const matchPassword = await bcrypt.compare(password, user.password);
+
+    if (!matchPassword) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    user.password = undefined;
+
+    res.cookie("access_token", token, { httpOnly: true }).status(201).json({
+      success: true,
+      message: "Signin successfull",
+      user,
+    });
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Signin failed, Please try again!!",
+    });
   }
 };
