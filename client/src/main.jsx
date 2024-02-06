7d052d97e027dd2dd24d872eb14dca312504a86c
N DIWAKAR <diwakarnath51@gmail.com>
2024-02-06 22:11:22 +0530

    added redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,16 +5,18 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { Provider } from "react-redux";
-import {store} from "./redux/store.js"
-
+import { persistor, store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-        <Toaster />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+          <Toaster />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
