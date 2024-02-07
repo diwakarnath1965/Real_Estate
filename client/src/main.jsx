f20fcf6cecdee56d54e8ef0a3fbac9b5c80cd930
N DIWAKAR <diwakarnath51@gmail.com>
2024-02-07 13:11:36 +0530

    complete image upload functionality

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,14 +9,12 @@ import { persistor, store } from "./redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <App />
-          <Toaster />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+        <Toaster />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
