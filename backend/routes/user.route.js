4b6db7d43709d61c6fabfb09898592da41246e94
N DIWAKAR <diwakarnath51@gmail.com>
2024-02-08 13:52:18 +0530

    created contact landlord function

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -4,7 +4,7 @@ import {
   getUserListings,
   test,
   updateUser,
-  
+  getUser
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
@@ -13,5 +13,6 @@ router.get("/", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 router.get("/listings/:id",verifyToken,getUserListings)
+router.get("/:id",verifyToken,getUser)
 
 export default router;
