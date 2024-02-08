095bfd69ab4921299247570e7fae5ee72953f9cd
N DIWAKAR <diwakarnath51@gmail.com>
2024-02-08 12:57:52 +0530

    added image slider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,36 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import About from './pages/About.jsx'
-import SignIn from './pages/SignIn.jsx'
-import SignUp from './pages/SignUp.jsx'
-import Header from './components/Header.jsx'
-import Profile from './pages/Profile.jsx'
-import PrivateRoute from './components/PrivateRoute.jsx'
-import CreateListing from "./pages/CreateListing.jsx"
-import UpdateListing from './pages/UpdateListing.jsx'
-
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import Header from "./components/Header.jsx";
+import Profile from "./pages/Profile.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
+import CreateListing from "./pages/CreateListing.jsx";
+import UpdateListing from "./pages/UpdateListing.jsx";
+import Listing from "./pages/Listing.jsx";
 
 const App = () => {
   return (
-    <div className='font-inter'>
-      <Header/>
+    <div className="font-inter">
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/sign-in' element={<SignIn/>}/>
-        <Route path='/sign-up' element={<SignUp/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route element={<PrivateRoute/>}>
-        <Route path='/profile' element={<Profile/>}/>
-        <Route path='/create-listing' element={<CreateListing/>}/>
-        <Route path='/update-listing/:id' element={<UpdateListing/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/update-listing/:id" element={<UpdateListing />} />
         </Route>
+
+        <Route path="/listing/:id" element={<Listing/>} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/pages/Listing.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/swiper-bundle.css";
+
+const Listing = () => {
+  SwiperCore.use([Navigation]);
+
+  const params = useParams();
+
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchListing = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(`/api/v1/listing/get/${params.id}`);
+        const data = await res.json();
+
+        if (data.success === false) {
+          setError(true);
+          setLoading(false);
+          return;
+        }
+        setListing(data);
+        setLoading(false);
+        setError(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchListing();
+  }, [params.id]);
+
+  return (
+    <main>
+      {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
+      {error && (
+        <p className="text-center my-7 text-2xl">Something went wrong</p>
+      )}
+      {listing && !loading && !error && (
+        <>
+          <Swiper navigation>
+            {listing.imageUrls.map((url) => (
+              <SwiperSlide key={url}>
+                <div
+                  className="h-[550px]"
+                  style={{
+                    background: `url(${url}) center no-repeat`,
+                    backgroundSize: "cover",
+                  }}
+                ></div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </>
+      )}
+    </main>
+  );
+};
+
+export default Listing;
